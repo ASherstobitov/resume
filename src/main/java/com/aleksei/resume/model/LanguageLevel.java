e74c36da15063f29e2a6dc00b3ada82dda93b942
Alexey Sherstobitov <aesherstobitov7@gmail.com>
2021-01-18 18:37:17 +0500

    DB configuration, TestDbGenerator.

---
+++ src/main/java/com/aleksei/resume/model/LanguageLevel.java
@@ -0,0 +1,24 @@
+package com.aleksei.resume.model;
+
+
+public enum LanguageLevel {
+
+    BEGINNER,
+
+    ELEMENTARY,
+
+    PRE_INTERMEDIATE,
+
+    INTERMEDIATE,
+
+    UPPER_INTERMEDIATE,
+
+    ADVANCED,
+
+    PROFICIENCY;
+
+    public String getDbValue() {
+        return name().toLowerCase();
+    }
+
+}
